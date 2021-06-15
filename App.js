2733da265f243e7c34c964e981c10991a2d686d9
Daniel Lay <dlay@brandeis.edu>
2021-06-15 07:46:00 -0700

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,151 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState }  from 'react';
+import { Button, StyleSheet, Text, View, Image, TextInput, SafeAreaView  } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+import LearnTo from './components/LearnTo'
+import MaxLift from './components/MaxLift'
+import BMIcalc from './components/BmiCalculator'
+
+const Stack = createStackNavigator();
+
+const Header = (props) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text style={styles.helloText}>
+      {props.text}
+      </Text>
     </View>
+    )
+}
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={IntroScreen}/>
+        <Stack.Screen name="Squat" component= {Squats}/>
+        <Stack.Screen name="Deadlift" component={Deadlift}/>
+        <Stack.Screen name="MaxLift" component={MaximumLift}/>
+        <Stack.Screen name="MaxDeadlift" component={MaxDeadlift}/>
+        <Stack.Screen name="BMI" component={BMI}/>
+        <Stack.Screen name="About" component={About}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+const IntroScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Header text='Get Swole. The App.'/>
+      <Text>This is a fitness app that looks at your form in real time and guides you on the perfect squat!</Text>
+      <Button title="Learn to Squat"
+        onPress={() =>
+          navigation.navigate('Squat', { name: 'Learn to Squat' })
+        }/>
+      <Button title="Learn to Deadlift"
+        onPress={() =>
+          navigation.navigate('Deadlift', {name: 'Learn to Deadlift'})
+        }/>
+      <Button title="Ideal Lifting Weight"
+        onPress={() =>
+          navigation.navigate('MaxLift', {name: 'Maximum Lifting'})
+        }/>
+      <Button title="BMI Calculator"
+        onPress={() =>
+          navigation.navigate('BMI', {name: 'BMI Calculator'})
+        }/>
+      <Button title="About"
+        onPress={() =>
+          navigation.navigate('About', {name: 'About'})
+        }/>
+    </View>
+  );
+};
+const Squats = ({ navigation, route }) => {
+  return (
+    <LearnTo url='https://i2.wp.com/powerfullyyoufitness.com/wp-content/uploads/2018/01/Proper-Squat-Form.png?ssl=1'/>
+  )
+};
+const Deadlift = ({ navigation, route }) => {
+  return (
+    <LearnTo url='https://i1.wp.com/physicalculturestudy.com/wp-content/uploads/2016/01/romaniandeadlift1.jpg?resize=563%2C331&ssl=1'/>
+  )
+};
+const MaximumLift = ({ navigation, route }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>This is a loose guide for the weight to start for each different lift</Text>
+      <Button title="Deadlift"
+        onPress={() =>
+          navigation.navigate('MaxDeadlift', {name: 'Maximum MaxDeadlift'})
+        }/>
+    </View>
+  )
+};
+const MaxDeadlift = ({ navigation, route }) => {
+  const [weight, setWeight] = useState(0)
+  return (
+    <View style={styles.container}>
+      <View style={styles.rowContainer}>
+        <Text>Enter weight</Text>
+        <TextInput
+              style={styles.textinput}
+              onChangeText={weight => {setWeight(parseFloat(weight))}}
+        />
+      </View>
+      <MaxLift lift='Deadlift' weight={weight}/>
+    </View>
+  )
+}
+const BMI = ({ navigation, route }) => {
+  return (
+    <BMIcalc/>
+  )
+}
+const About = ({ navigation, route }) => {
+  const [text, onChangeText] = React.useState(" Leave Feedback...");
+  return (
+    <View style={styles.container}>
+      <Text>Created by Daniel Lay</Text>
+      <Text>Email: dlay@brandeis.edu</Text>
+      <SafeAreaView>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+        />
+      </SafeAreaView>
+    </View>
+  )
+};
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection:'column',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  rowContainer: {
+    flexDirection:'row',
+  },
+  helloText: {
+    fontSize: 40,
+    color: 'blue'
+  },
+  logo:{
+    width: 1526,
+    height: 900,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
   },
 });
